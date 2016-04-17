f44e3dcfeed290738180fd944a34b35537e9d2d7
lumagatesvc001 <ryan.irujo@lumagate.com>
2016-04-17 16:38:32 +0000

    First Commit from Jenkins Server

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello Leeroy Jenkins', function(done) {
-    request(app).get('/').expect('Leeroy Jenkins - first node.js app', done);
+    request(app).get('/').expect('Leeroy Jenkins - first commit - first node.js app', done);
   });
 });
