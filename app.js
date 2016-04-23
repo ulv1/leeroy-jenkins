d88b95973146a3136ab58fd862528eed6d778803
Ryan Irujo <ryan.irujo@gmail.com>
2016-04-23 15:26:05 +0200

    updated app.js response

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('Hello Jenkins M - Leeroy Jenkins - first node.js app');
+  res.send('Hello Jenkins MM - Leeroy Jenkins - first node.js app');
 });
  
 app.listen(process.env.PORT || 5000);
